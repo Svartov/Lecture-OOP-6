5b481c6a515527ad2bbc8e26a6dd9797b8c81181
Svartov <100573588+Svartov@users.noreply.github.com>
2022-12-25 17:50:51 +0100

    Update CalculateSum.java

--- 6.2/CalculateSum.java
+++ 6.2/CalculateSum.java
@@ -2,7 +2,7 @@ package Leks6;
 
 public class CalculateSum {
 
-    public static long CalculateSum(int[] arrayNum) {
+    public static long calculateSum(int[] arrayNum) {
 
         long sum = 0;
         for (int i = 0; i < arrayNum.length; i++) {
